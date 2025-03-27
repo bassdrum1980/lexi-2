28110586f0b8fbf5d75fe2f757e71909bbde7e58
Sasha Shevchenko <acidi@icloud.com>
2025-03-26 22:28:01 -0700

    adds authApi and redux-logger as middlewares

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
+import logger from 'redux-logger';
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
+import * as authApi from '../api/auth';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
-})
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: authApi, // Provide authApi as an extra argument
+      },
+    }).concat(logger),
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
