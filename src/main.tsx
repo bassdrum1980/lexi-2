59e872bf0aa006c409972a3ac837eabd8e4aae51
Sasha Shevchenko <acidi@icloud.com>
2025-04-02 11:57:56 -0700

    moves protectedRoute component to shared location

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 import { store } from './app/store.ts'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import ProtectedRoute from './pages/auth/ProtectedRoute.tsx'
+import ProtectedRoute from './components/protected/ProtectedRoute.tsx'
 import SignInPage from './pages/auth/SignIn.tsx'
 import HomePage from './pages/home/Home.tsx'
 import * as appRoutes from './routes/';
