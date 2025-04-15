08c3d35ee572c40f0cdfa19e064a99552b09d56a
Sasha Shevchenko <acidi@icloud.com>
2025-04-15 15:57:38 -0700

    moves routing into a reusable component

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,34 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import ProtectedRoute from './components/protected/ProtectedRoute.tsx';
+import SignInPage from './pages/auth/SignIn.tsx';
+import HomePage from './pages/home/Home.tsx';
+import App from './App.tsx';
+import { signinURL } from './routes/index.ts';
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<App />}
+      >
+        {/* Protected Routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route
+            index
+            element={<HomePage />}
+          />
+        </Route>
+
+        {/* Public Routes */}
+        <Route
+          path={signinURL} // e.g., '/signin'
+          element={<SignInPage />}
+        />
+      </Route>
+
+      {/* Fallback route - redirects any unmatched path to the root */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+};
