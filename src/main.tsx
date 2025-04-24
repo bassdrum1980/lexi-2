15c99b37230302a50056bd49eb239047c83b5965
Sasha Shevchenko <acidi@icloud.com>
2025-04-24 12:05:22 -0700

    removes non-null assertion

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes/AppRoutes.tsx';
 import './styles/index.scss';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>
-);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </Provider>
+    </StrictMode>
+  );
+} else {
+  console.error("Root element with ID 'root' not found in the DOM.");
+}
