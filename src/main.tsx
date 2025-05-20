25fb0b17f22d1d522fc1191a4fcb46669b58ebfb
Sasha Shevchenko <acidi@icloud.com>
2025-05-20 10:44:14 -0700

    gets rid of generic css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import { store } from './app/store.ts';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
